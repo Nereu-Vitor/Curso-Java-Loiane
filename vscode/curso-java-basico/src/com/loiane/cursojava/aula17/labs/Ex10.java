d4923e8d37d71e18fde00edf14ead9b995c217b2
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-14 20:59:11 -0300

    Aula 17: Finaliza exercício 10

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex10.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex10.java
@@ -1,5 +1,24 @@
 package com.loiane.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Ex10 {
 
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int num1 = scan.nextInt(); 
+
+        System.out.print("Digite o segundo número: ");
+        int num2 = scan.nextInt();
+
+        System.out.println("Números dentro do intervalo fornecido: ");
+
+        for (int i = num1; i <= num2; i++) {
+            System.out.println(i);
+        }
+
+    }
 }
