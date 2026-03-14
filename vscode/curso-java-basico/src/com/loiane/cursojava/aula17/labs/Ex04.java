6295caf66c7200494de7d17b519cf37a442a7afa
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-14 20:55:28 -0300

    Aula 17: Finaliza exercício 04

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex04.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex04.java
@@ -2,4 +2,21 @@ package com.loiane.cursojava.aula17.labs;
 
 public class Ex04 {
 
+    public static void main(String[] args) {
+        
+
+        int popA = 80000, popB = 200000;
+        int anos = 0;
+
+        while (popA <= popB) {
+            popA += (popA * 3 / 100);
+            popB += (popB * 1.5 / 100);
+            anos++;
+        }
+
+        System.out.println("População A: " + popA);
+        System.out.println("População B: " + popB);
+        System.out.println("Quantidade de Anos: " + anos);
+
+    }
 }
