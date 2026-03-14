eda2f40bd74d0fd237e0e1219370227c40e77be3
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-14 20:56:08 -0300

    Aula 17: Finaliza exercício 06

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex06.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex06.java
@@ -2,4 +2,18 @@ package com.loiane.cursojava.aula17.labs;
 
 public class Ex06 {
 
+    public static void main(String[] args) {                
+
+        // Exibe os números um abaixo do outro
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+
+        // Exibe os números um ao lado do outro
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+    }
 }
