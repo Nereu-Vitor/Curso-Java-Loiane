eb821f8a7d47d2e0be1c633562a3c2a90710cc92
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-14 20:59:28 -0300

    Aula 17: Finaliza exercício 11

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex11.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex11.java
@@ -1,5 +1,27 @@
 package com.loiane.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Ex11 {
 
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int num1 = scan.nextInt(); 
+
+        System.out.print("Digite o segundo número: ");
+        int num2 = scan.nextInt();
+
+        int soma = 0;
+        System.out.println("Números dentro do intervalo fornecido: ");
+
+        for (int i = num1; i <= num2; i++) {
+            System.out.println(i);
+            soma += i;
+        }
+
+        System.out.println("Soma dos números do intervalo: " + soma);
+    }
 }
