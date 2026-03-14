df120ecbbf998d121f585bb7791a8c7f97de41c6
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-14 20:59:37 -0300

    Aula 17: Finaliza exercício 12

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex12.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex12.java
@@ -1,5 +1,20 @@
 package com.loiane.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Ex12 {
 
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int num = scan.nextInt();
+
+        System.out.println("Tabuada de " + num + ":");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
 }
