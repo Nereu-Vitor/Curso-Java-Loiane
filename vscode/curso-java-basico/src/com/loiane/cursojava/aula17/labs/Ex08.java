11f34c9c45e1128f491edb2cc1a0b4c1bbd5f99b
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-14 20:57:06 -0300

    Aula 17: Finaliza exercício 08

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex08.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex08.java
@@ -1,5 +1,28 @@
 package com.loiane.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Ex08 {
 
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        int num;
+        int soma = 0;
+        double media = 0.0;
+
+        for (int i = 0; i < 5; i++) {
+            
+            System.out.print("Digite o " + (i + 1) + "° número: ");
+            num = scan.nextInt();
+
+            soma += num;
+        }
+
+        media = soma / 5;
+
+        System.out.println("Soma dos números: " + soma);
+        System.out.println("Média dos números: " + media);
+    }
 }
