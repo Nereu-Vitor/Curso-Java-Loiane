525000f3153d4123065478ffbb15a071e5b9c984
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-14 20:57:15 -0300

    Aula 17: Finaliza exercício 09

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex09.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex09.java
@@ -2,4 +2,15 @@ package com.loiane.cursojava.aula17.labs;
 
 public class Ex09 {
 
+    public static void main(String[] args) {
+        
+        System.out.println("Números ímpares entre 1 a 50: ");
+
+        for (int i = 1; i <= 50; i++) {
+            
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
