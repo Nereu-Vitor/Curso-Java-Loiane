e3f97e0d8cd366f5c3e9342c9f468c7914ea150c
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-15 12:31:35 -0300

    Aula 17: Finaliza exercício 24

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex24.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex24.java
@@ -2,4 +2,13 @@ package com.loiane.cursojava.aula17.labs;
 
 public class Ex24 {
 
+    public static void main(String[] args) {
+        
+        System.out.println("Preço do pão: R$ 0.18");
+        System.out.println("Panificadora Pão de Ontem - Tabela de Preços");
+
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(i + " - R$ " + (i * 0.18));
+        }
+    }
 }
