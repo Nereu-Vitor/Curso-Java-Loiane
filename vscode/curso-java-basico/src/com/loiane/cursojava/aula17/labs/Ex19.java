cef44c9b03c20f5885f477e79013648d8a90ae47
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-14 22:16:14 -0300

    Aula 17: Finaliza exercício 19

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex19.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex19.java
@@ -1,5 +1,30 @@
 package com.loiane.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Ex19 {
 
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de notas: ");
+        int notas = scan.nextInt();
+
+        double soma = 0.0;
+        double media;
+        double nota;
+
+        for (int i = 0; i < notas; i++) {
+            System.out.print("Digite a " + (i + 1) + "° nota: ");
+            nota = scan.nextDouble(); 
+            
+            soma += nota;
+        }
+
+        media = soma / notas;
+
+        System.out.println("Média das Notas: " + media);
+        
+    }
 }
