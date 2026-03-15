cc79351444d9001b5ff90286b8c46627f0bc8eda
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-15 12:22:46 -0300

    Aula 17: Finaliza exercício 22

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex22.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex22.java
@@ -1,5 +1,29 @@
 package com.loiane.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Ex22 {
 
+   public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Digite a quantidade de CDs: ");
+        int cds = scan.nextInt();
+
+        double preco;
+        double soma = 0;
+
+        for (int i = 1; i<= cds; i++) {
+
+            System.out.print("Digite o valor do " + i + "° cd: ");
+            preco = scan.nextDouble();
+
+            soma += preco;
+        }
+
+        double media = soma / cds;
+        
+        System.out.println("O preço médio gasto com cada CD é: R$" + media);
+   } 
 }
