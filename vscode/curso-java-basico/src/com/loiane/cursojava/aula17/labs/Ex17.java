a57e44b10a9335f7e24de4bec4b3287aa7862260
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-14 21:46:11 -0300

    Aula 17: Finaliza exercício 17

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex17.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex17.java
@@ -1,5 +1,25 @@
 package com.loiane.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Ex17 {
 
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int num = scan.nextInt();
+
+        System.out.print(num + "! = ");
+
+        long fatorial = 1;
+
+        for (int i = num; i > 1; i--) {
+            System.out.print(i + " x ");
+            fatorial *= i;
+        }
+
+        System.out.println("1 = " + fatorial);
+    }
 }
