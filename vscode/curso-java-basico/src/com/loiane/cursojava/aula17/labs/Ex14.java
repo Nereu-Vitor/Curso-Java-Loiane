18905acd4c0f064ea11549dd63881370b883286d
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-14 21:14:44 -0300

    Aula 17: Finaliza exercício 14

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex14.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex14.java
@@ -1,5 +1,29 @@
 package com.loiane.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Ex14 {
 
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        int pares = 0;
+        int impares = 0;
+        int num;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Digite o " + (i + 1) + "° número: ");
+            num = scan.nextInt();
+
+            if (num % 2 == 0) {
+                pares++;
+            } else {
+                impares++;
+            }
+        }        
+
+        System.out.println("Pares: " + pares);
+        System.out.println("Impares: " + impares);
+    }
 }
