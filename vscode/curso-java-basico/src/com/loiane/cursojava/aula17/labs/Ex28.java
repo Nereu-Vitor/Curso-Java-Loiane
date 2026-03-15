f95d7821dbe3225abfaeb03adaedf045d67d5d6b
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-15 17:50:30 -0300

    Aula 17: Finaliza exercício 28

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex28.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex28.java
@@ -1,5 +1,28 @@
 package com.loiane.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Ex28 {
 
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int num = scan.nextInt();
+
+        boolean ehPrimo = true;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                ehPrimo = false;
+            }
+
+        }
+
+        if (ehPrimo) {
+            System.out.println(num + " é primo");
+        } else {
+            System.out.println(num + " não é primo");
+        }
+    }
 }
--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex29.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex29.java
@@ -1,5 +1,36 @@
 package com.loiane.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Ex29 {
 
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o número que deseja exibir a tabuada: ");
+        int num = scan.nextInt();
+
+        System.out.print("Digite em qual número deve começar a tabuada: ");
+        int comeco = scan.nextInt();
+
+        boolean numValido = false;
+        int fim;
+        do {
+            System.out.print("Digite e qual número deve terminar: ");
+            fim = scan.nextInt();
+
+            if (fim >= comeco) {
+                numValido = true;
+            } else {
+                System.out.println("Número inválido. O fim não deve ser menor que o começo. Digite novamente.");
+            }
+
+        } while (!numValido);
+
+        System.out.println("Vou montar a tabuda de " + num + " começando em " + comeco + " e terminando em " + fim);
+        for (int i = comeco; i <= fim; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
 }
