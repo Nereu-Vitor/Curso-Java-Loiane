f2a12509dc9a1ddd2ebf0bb394efba76eb9baa6a
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-15 19:20:32 -0300

    Aula 17: Finaliza exercício 34

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex34.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex34.java
@@ -1,5 +1,22 @@
 package com.loiane.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Ex34 {
 
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o valor de n: ");
+        int n = scan.nextInt();
+
+        double soma = 0;
+
+        for (int i =1; i <= n; i++) {
+            soma += 1 / i;
+        }
+
+        System.out.println("Soma = " + soma);
+    }
 }
