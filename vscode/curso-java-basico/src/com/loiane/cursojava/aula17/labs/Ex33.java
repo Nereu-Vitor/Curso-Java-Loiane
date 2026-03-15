0373ee45a3df0694f13a92d07da16124914c6328
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-15 19:17:58 -0300

    Aula 17: Finaliza exercício 33

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex33.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex33.java
@@ -1,5 +1,25 @@
 package com.loiane.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Ex33 {
 
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o valor de n: ");
+        int n = scan.nextInt();
+
+        double soma = 0;
+        int i = 1;
+        int j = 1;
+
+        for (; i < n; i++, j += 2) {
+            System.out.print(i + "/" + j + " + ");
+            soma += i / j;
+        }
+
+        System.out.print(i + "/" + j + " = " + soma + "\n");
+    }
 }
