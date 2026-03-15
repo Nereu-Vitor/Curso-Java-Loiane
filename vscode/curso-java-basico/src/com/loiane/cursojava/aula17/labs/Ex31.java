40a7b8f2895f015891e08914c37ab6e5e50112bb
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-15 18:13:42 -0300

    Aula 17: Finaliza exercício 31

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex31.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex31.java
@@ -1,5 +1,24 @@
 package com.loiane.cursojava.aula17.labs;
 
+import java.text.DecimalFormat;
+
 public class Ex31 {
 
+    public static void main(String[] args) {        
+
+        double salario = 1000;
+        double percentual = 1.5;
+
+        salario += (salario * percentual / 100);
+
+        DecimalFormat format = new DecimalFormat("###,###.##");
+
+        for (int i = 1997; i <= 2026; i++) {
+            percentual *= 2;
+            
+            salario += (salario * percentual / 100);
+
+            System.out.println(i + " = " + format.format(salario) + " - " + percentual + "%");
+        }
+    }
 }
