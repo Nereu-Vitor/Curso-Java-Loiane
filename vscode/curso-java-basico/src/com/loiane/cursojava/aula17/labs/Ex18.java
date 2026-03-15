4a081f87d66ad5315e5733c76df9cd9b3b2641b3
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-14 21:56:54 -0300

    Aula 17: Finaliza exercício 18

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex18.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex18.java
@@ -1,5 +1,33 @@
 package com.loiane.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Ex18 {
 
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int num = scan.nextInt();
+        boolean ehPrimo = true;
+
+        if (num < 2) {
+            ehPrimo = false;
+        } else {
+            for (int i = 2; i < num; i++) {
+
+                if (num % i == 0) {
+                    ehPrimo = false;
+                }
+
+            }
+        }
+
+        if (ehPrimo) {
+            System.out.println(num + " é primo");
+        } else {
+            System.out.println(num + " não é primo");
+        }
+    }
 }
