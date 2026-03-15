a983c3f4c534770e8bdec34622d2abdd13bd032b
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-15 13:00:04 -0300

    Aula 17: Finaliza exercício 26

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex26.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex26.java
@@ -1,5 +1,26 @@
 package com.loiane.cursojava.aula17.labs;
 
-public class Ex26 {
+import java.util.Scanner;
 
+public class Ex26 {
+ 
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int num = scan.nextInt();
+
+        System.out.println("Fatorial de: " + num);
+        System.out.print(num + "! = ");
+
+        long fatorial = 1;
+
+        for (int i = num; i > 1; i--) {
+            System.out.print(i + " . ");
+            fatorial *= i;
+        }
+
+        System.out.println("1 = " + fatorial);
+    }
 }
