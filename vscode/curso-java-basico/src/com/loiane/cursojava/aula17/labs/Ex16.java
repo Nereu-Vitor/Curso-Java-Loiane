4ccefe826aa725f8b72e369ba9c07d12ced8768e
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-14 21:33:20 -0300

    Aula 17: Finaliza exercício 16

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex16.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex16.java
@@ -2,4 +2,30 @@ package com.loiane.cursojava.aula17.labs;
 
 public class Ex16 {
 
+    public static void main(String[] args) {
+
+        int termo1 = 0;
+        int termo2 = 1;
+        int termo3;
+
+        System.out.print(termo1 + " -> " + termo2 + " -> ");
+
+        boolean alcancou = false;
+        do {
+
+            termo3 = termo1 + termo2;
+                        
+            System.out.print(termo3 + " -> ");
+            
+            if (termo3 > 500) {
+                alcancou = true;
+            }
+
+            termo1 = termo2;
+            termo2 = termo3;
+
+        } while (!alcancou);
+
+        System.out.println("FIM");
+    }
 }
