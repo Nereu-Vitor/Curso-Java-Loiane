314facdfd7e6366ac2c07ebffd658de66db90ec8
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-15 12:28:19 -0300

    Aula 17: Finaliza exercício 23

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex23.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex23.java
@@ -2,4 +2,12 @@ package com.loiane.cursojava.aula17.labs;
 
 public class Ex23 {
 
+    public static void main(String[] args) {
+        
+        System.out.println("Loja Quase Dois - Tabela de Preços");
+
+        for (int i = 1; i <= 50; i++) {            
+            System.out.println(i + " - R$" + (i * 1.99));
+        }
+    }
 }
