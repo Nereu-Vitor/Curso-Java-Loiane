5ab464b1e7beab4fb302bffb642497e68c7fa511
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-15 17:55:18 -0300

    Aula 17: Finaliza exercício 29

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex29.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex29.java
@@ -5,32 +5,28 @@ import java.util.Scanner;
 public class Ex29 {
 
     public static void main(String[] args) {
-        
+
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Digite o número que deseja exibir a tabuada: ");
+        System.out.print("Digite um número: ");
         int num = scan.nextInt();
 
-        System.out.print("Digite em qual número deve começar a tabuada: ");
-        int comeco = scan.nextInt();
+        boolean ehPrimo;
+        for (int i = 2; i <= num; i++) {
 
-        boolean numValido = false;
-        int fim;
-        do {
-            System.out.print("Digite e qual número deve terminar: ");
-            fim = scan.nextInt();
-
-            if (fim >= comeco) {
-                numValido = true;
-            } else {
-                System.out.println("Número inválido. O fim não deve ser menor que o começo. Digite novamente.");
+            ehPrimo = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    ehPrimo = false;
+                }
             }
 
-        } while (!numValido);
-
-        System.out.println("Vou montar a tabuda de " + num + " começando em " + comeco + " e terminando em " + fim);
-        for (int i = comeco; i <= fim; i++) {
-            System.out.println(num + " x " + i + " = " + (num * i));
+            if (ehPrimo) {
+                System.out.println(i + " é primo");
+            } else {
+                System.out.println(i + " não é primo");
+            }
         }
+
     }
 }
