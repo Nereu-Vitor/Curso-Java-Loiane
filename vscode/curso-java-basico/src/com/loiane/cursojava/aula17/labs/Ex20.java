c07cc3e4ec495b82c9bce97e38713b7ffb6d77cb
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-15 10:27:55 -0300

    Aula 17: Finaliza exercício 20

--- vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex20.java
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula17/labs/Ex20.java
@@ -1,5 +1,37 @@
 package com.loiane.cursojava.aula17.labs;
 
+import java.util.Scanner;
+
 public class Ex20 {
 
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de idades: ");
+        int idades = scan.nextInt();
+
+        int idade;
+        int soma = 0;
+
+        for (int i = 0; i < idades; i++) {
+            System.out.print("Digite a " + (i + 1) + "ª idade: ");
+            idade = scan.nextInt();
+
+            soma = +idade;
+        }
+
+        double media = soma / idades;
+
+        System.out.println("A média de idade da turma é: " + media);
+        
+        if (media <= 25) {
+            System.out.println("Turma Jovem");
+        } else if (media <= 60) {
+            System.out.println("Turma Adulta");
+        } else {
+            System.out.println("Turma Idosa");
+        }
+
+    }
 }
