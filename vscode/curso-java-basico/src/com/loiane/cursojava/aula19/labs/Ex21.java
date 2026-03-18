dbaadbfedb3acde9f655d9c3484faaa195f1f401
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-18 18:29:22 -0300

    Aula 19: Finaliza exercício 21 (N/A)

---
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula19/labs/Ex21.java
@@ -0,0 +1,14 @@
+package com.loiane.cursojava.aula19.labs;
+
+import java.util.Scanner;
+
+public class Ex21 {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        // Exercício não aplicável/existente na lista da aula.
+        System.out.println("Exercício 21: N/A");
+    }
+}
