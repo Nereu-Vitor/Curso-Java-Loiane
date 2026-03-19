5e4baac89fcf0b09efb971f995aa7c24b1b80048
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-19 01:01:02 -0300

    Aula 19: Finaliza exercício 25

---
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula19/labs/Ex25.java
@@ -0,0 +1,34 @@
+package com.loiane.cursojava.aula19.labs;
+
+import java.util.Scanner;
+
+public class Ex25 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] vetorA = new int[10];
+        int[] vetorB = new int[vetorA.length];
+
+        for (int i = 0; i < vetorA.length; i++) {
+
+            System.out.printf("Digite um número para a posição %d: ", i);
+            vetorA[i] = scan.nextInt();
+
+            vetorB[i] = (vetorA[i] % 2 == 0) ? 1 : 0;
+        }
+
+        System.out.print("Vetor A = ");
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.printf("%d ", vetorA[i]);
+        }
+        System.out.println();
+
+        System.out.print("Vetor B = ");
+        for (int i = 0; i < vetorB.length; i++) {
+            System.out.printf("%d ", vetorB[i]);
+        }
+        System.out.println();
+    }
+}
