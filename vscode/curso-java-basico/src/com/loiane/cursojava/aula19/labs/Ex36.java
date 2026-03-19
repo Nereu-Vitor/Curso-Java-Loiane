bcdb6ee3397bc0e9e54f469e5730889d9300151c
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-19 19:21:59 -0300

    Aula 19: Finaliza exercício 36

---
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula19/labs/Ex36.java
@@ -0,0 +1,24 @@
+package com.loiane.cursojava.aula19.labs;
+
+import java.util.Scanner;
+
+public class Ex36 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        double[] vetorA = new double[11];
+
+        for (int i = 0; i < vetorA.length; i++) {
+
+            vetorA[i] = Math.pow(2, i);
+        }
+
+        System.out.print("Vetor A = ");
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.printf("%.1f ", vetorA[i]);
+        }
+        System.out.println();
+    }
+}
