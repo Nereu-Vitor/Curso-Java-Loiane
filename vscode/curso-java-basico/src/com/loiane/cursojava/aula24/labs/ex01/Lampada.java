da10a61e918a8136a01d685f4214c77853128baa
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-22 14:47:04 -0300

    feat(aula24): resolver exercício 01 (Lampada)

---
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula24/labs/ex01/Lampada.java
@@ -0,0 +1,14 @@
+package com.loiane.cursojava.aula24.labs.ex01;
+
+public class Lampada {
+
+    public String modelo;
+    public String tensao;
+    public int potencia;
+    public String cor;
+    public String tipoLuz;
+    public int garantiaMeses;
+    public String[] tipos;
+    public boolean tipoAbajur;
+
+}
