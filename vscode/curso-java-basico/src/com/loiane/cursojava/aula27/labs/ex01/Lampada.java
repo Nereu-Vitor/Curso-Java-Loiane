b0bed20ce430f95d8a575d4163b5e18097633742
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-22 21:58:45 -0300

    feat(aula27): resolver exercício 01 (Lampada)

---
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula27/labs/ex01/Lampada.java
@@ -0,0 +1,40 @@
+package com.loiane.cursojava.aula27.labs.ex01;
+
+public class Lampada {
+
+    public String modelo;
+    public String tensao;
+    public int potencia;
+    public String cor;
+    public String tipoLuz;
+    public int garantiaMeses;
+    public String[] tipos;
+    public boolean tipoAbajur;
+
+    public boolean ligada;
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+
+    public void mostrarEstado() {
+        if (ligada) {
+            System.out.println("Lâmpada está ligada");
+        } else {
+            System.out.println("Lâmpada está desligada");
+        }
+    }
+
+    public void mudarEstado() {
+        if (ligada) {
+            desligar();
+        } else {
+            ligar();
+        }
+    }
+
+}
