5ea9de77a83be85d720f60ef0d5c2845ef9b18cd
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-24 00:39:37 -0300

    feat(aula28): definir estrutura de pacotes e praticar imports

---
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula28/Pacotes.java
@@ -0,0 +1,21 @@
+package com.loiane.cursojava.aula28;
+
+import com.loiane.cursojava.aula20.labs.Ex04;
+import com.loiane.cursojava.aula20.labs.Ex05;
+import com.loiane.cursojava.aula20.labs.Ex06;
+import com.loiane.cursojava.aula27.Carro;
+
+public class Pacotes {
+
+    public static void main(String[] args) {
+        
+        Carro carro;
+        Carro carro2;
+        
+        Ex06 ex06;
+        Ex05 ex05;
+        Ex04 ex04; 
+        
+    }
+
+}
