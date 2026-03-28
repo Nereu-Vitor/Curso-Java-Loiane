409482934d13a7c9db393923871554f5e930576e
Nereu Vítor <nereuvitor12@gmail.com>
2026-03-28 15:32:18 -0300

    feat(aula39): demonstrar visibilidade protected entre pacotes diferentes

---
+++ vscode/curso-java-basico/src/com/loiane/cursojava/aula39/teste/Teste.java
@@ -0,0 +1,13 @@
+package com.loiane.cursojava.aula39.teste;
+
+import com.loiane.cursojava.aula39.Pessoa;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        
+        Pessoa pessoa = new Pessoa();
+        // pessoa.nomeVisibilidade = "Tentando acesso."; Vai dar erro já que a classe Teste não herda de Pessoa
+
+    }
+}
